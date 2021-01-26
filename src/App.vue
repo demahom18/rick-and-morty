33d6b2b1b19ca3ca2b4f4a9dae7a6bd749f0df6e
Mohamed Donzo <demahom18@outlook.com>
2021-01-25 22:44:31 -0500

    feat: playgrid done

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="header-img">
+    <img 
+      src="./assets/Rick_and_Morty_logo.png" 
+      alt="Rick and morty"
+    />
+  </div>
+  <div class="scoreboard">
+    <img src="./assets/morty.png" alt="morty">
+    <span>SCORE : {{ score }} Level: {{ level }}</span>
+  </div>
+  <div class="welcome-text">
+    CLICK <span @click="startGame">PLAY</span> TO START PLAYING 
+  </div>
+  <div class="playgrid" >
+    <RickMorty  v-for="n in 9" :key="n"/>
+  </div>
+</template>
+
+<script>
+import RickMorty from './components/RickMorty.vue';
+
+export default {
+  name: 'App',
+  components: {
+    RickMorty
+  },
+  data (){
+    return {
+      score: 0,
+      level: 1
+    }
+  }, 
+}
+</script>
+
+<style lang="scss">
+#app{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .header-img img{
+    width:300px;
+    height:auto;
+    margin: 10px auto;
+  }
+  .scoreboard {
+    margin: 0 auto;
+    display: flex;
+    font-size: 2rem;
+    flex-direction: column;
+    img {
+      height:auto;
+      margin: 0 auto;
+      max-width:120px;
+    }
+  }
+  .playgrid {
+    display: grid;
+    grid-template-columns: repeat(3, 150px);
+    grid-template-rows:  repeat(3, 130px);
+    gap:10px;
+  }
+  .welcome-text{
+    font-size: 2.3rem;
+    color: #5b9d47;
+    span {
+      color: #02a2b7;
+      opacity: .6;
+      cursor:pointer;
+      text-decoration: underline;
+      transition: .3s;
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
+}
+
+</style>
+
---
+++ src/components/RickMorty.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hole">
+    <div class="cloud">
+        <img src="../assets/cloud.png" alt="cloud">
+    </div>
+    <div class="rick">
+        <img src="../assets/rick.png" alt="rick">
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+.hole {
+    position: relative;
+    overflow: hidden;
+    img{
+        height:auto;
+        max-width:150px;
+        min-width: 75px;
+        position: absolute;
+    }
+    .cloud img{
+        bottom:-20px;
+        z-index:50; 
+    }
+    .rick img{
+        transform:translateY(80%);
+        left: 15px;
+        min-width: 50px;
+        max-width:100px;
+    }
+    &.up .rick img {
+        transition: .1s;
+        transform:translateY(0);
+    }
+}
+
+
+</style>
